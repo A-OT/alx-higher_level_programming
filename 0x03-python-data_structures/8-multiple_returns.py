fa564d1a7f2da5a27e5df31a152b85f2a91c1672
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-08-29 08:50:37 -0700

    Task added

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return (0, None)
+    len_sen = len(sentence)
 
-    return (len(sentence), sentence[0])
+    if (len_sen == 0):
+        new_tuple = (len_sen, None)
+    else:
+        new_tuple = (len_sen, sentence[0])
+
+    return (new_tuple)
