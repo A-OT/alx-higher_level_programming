cf0b8c347b410af78717bdd827031325e6bd8c17
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-08-29 08:08:33 -0700

    Task solution added

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j != 0:
-                print(" ", end='')
-            print("{:d}".format(matrix[i][j]), end='')
-        print()
+def no_c(my_string):
+
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
