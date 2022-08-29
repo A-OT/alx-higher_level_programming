253ae9f2416b95a064fb350570632e833500d938
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-08-29 08:54:28 -0700

    Task added

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
+def max_integer(my_list=[]):
 
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+
+    return (big)
